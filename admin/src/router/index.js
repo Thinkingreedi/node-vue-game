48226b6912287aaeab20fb779aa259919fa2ee6a
A-SHIYI <2310268377@qq.com>
2022-07-19 15:56:36 +0800

    实现了分类的增删查改

---
+++ admin/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
+import CategoryEdit from '../views/CategoryEdit.vue'
+import CategoryList from '../views/CategoryList.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: Main,
+    children: [
+      { path: '/categories/create', component: CategoryEdit },
+      { path: '/categories/edit/:id', component: CategoryEdit, props: true },
+      { path: '/categories/list', component: CategoryList }
+    ]
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
