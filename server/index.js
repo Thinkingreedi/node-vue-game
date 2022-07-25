4cd9c7474107c99bffea402e40e3195810afe101
A-SHIYI <2310268377@qq.com>
2022-07-25 09:01:40 +0800

    部署

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ app.set('secret', 'q3w4e5r6t7')
 // 跨域
 app.use(require('cors')())
 app.use(express.json())
+app.use('/admin', express.static(__dirname + '/admin'))
+app.use('/', express.static(__dirname + '/web'))
 app.use('/uploads', express.static(__dirname + '/uploads'))
+
 
 require('./plugins/db')(app)
 require('./routes/admin')(app)
--- web/vue.config.js
+++ web/vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
+	outputDir: __dirname + '/../server/web',
+	// publicPath: process.env.NODE_ENV === 'production' ? '/web/' : '/',
 	lintOnSave: false, //关闭语法检查
 }
