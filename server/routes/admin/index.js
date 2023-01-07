0c85bd3f857a8db703bac5435691d184b6cf9cd5
Thinkingreedi <2310268377@qq.com>
2023-01-07 19:23:11 +0800

    1.7 修复

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -45,7 +45,7 @@ module.exports = app => {
     const upload = multer({ dest: __dirname + '/../../uploads' })
     app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
         const file = req.file
-        file.url = `http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://moba.thinkingreedi.xyz/uploads/${file.filename}`
         res.send(file)
     })
     app.post('/admin/api/login', async (req, res) => {
